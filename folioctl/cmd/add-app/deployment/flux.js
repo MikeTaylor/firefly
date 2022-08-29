96516bbc52a3e40d483efa903796aca11fb6428f
Mike Taylor <mike@indexdata.com>
2022-08-29 17:10:05 +0200

    New

---
+++ folioctl/cmd/add-app/deployment/flux.js
@@ -0,0 +1,3 @@
+// XXX to be done
+// See git wrangling in Ansible playbook at
+// https://github.com/indexdata/id-folio-infrastructure/blob/f1b65bf20d42d394d2a5d71eafc2393a0e1b9016/ansible/create-tenants.yml#L43
