5baaa3d884d33c0142a2ee54d8ddf80f4655dc84
Mike Taylor <mike@indexdata.com>
2022-08-29 17:13:01 +0200

    Add another link

--- folioctl/cmd/add-app/deployment/flux.js
+++ folioctl/cmd/add-app/deployment/flux.js
@@ -1,3 +1,6 @@
 // XXX to be done
-// See git wrangling in Ansible playbook at
+
+// See git wrangling in Ansible playbook at:
 // https://github.com/indexdata/id-folio-infrastructure/blob/f1b65bf20d42d394d2a5d71eafc2393a0e1b9016/ansible/create-tenants.yml#L43
+// See aso an example of a git repo containing Kubernetes manifests:
+// https://github.com/indexdata/eks-folio-us-east-1-1
