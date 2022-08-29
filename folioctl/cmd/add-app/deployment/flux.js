52ed4b5d530de967b991244dcf2cad07c778554e
Mike Taylor <mike@indexdata.com>
2022-08-29 17:19:28 +0200

    Add another link

--- folioctl/cmd/add-app/deployment/flux.js
+++ folioctl/cmd/add-app/deployment/flux.js
@@ -4,3 +4,5 @@
 // https://github.com/indexdata/id-folio-infrastructure/blob/f1b65bf20d42d394d2a5d71eafc2393a0e1b9016/ansible/create-tenants.yml#L43
 // See aso an example of a git repo containing Kubernetes manifests:
 // https://github.com/indexdata/eks-folio-us-east-1-1
+// When depoying a new module, I need to create a directory like
+// https://github.com/indexdata/eks-folio-us-east-1-1/tree/master/duke-test/folio-modules/mod-calendar-1.14.0
